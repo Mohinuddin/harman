a8b5b3ffd243a4ffcaff255b806230815acaca79
Mohinuddin <mohinuddin.khan@gmail.com>
2016-02-21 18:05:05 +0530

    updating shell and test

--- shell.sh
+++ shell.sh
@@ -6,10 +6,3 @@ do_something $retval
 if [ $retval -ne 0 ]; then
     echo "Return code was not zero but $retval"
     fi
-retval=$?
-retval=$?
-retval=$?
-retval=$?
-retval=$?
-retval=$?
-retval=$?
